74cc7ac8c3f2b63af3045a65f16ab92847157dd1
jinjin <jinjin.xu@gsma.com>
2020-06-04 17:11:22 +0100

    add options for webpack and test

--- bin/index.js
+++ bin/index.js
@@ -1,20 +1,25 @@
 #!/usr/bin/env node
 
-const yargs = require("yargs");
+const yargs = require('yargs');
 const ejs = require('ejs');
 const fs = require('fs');
+const rimraf = require('rimraf');
+
+const PLUGIN_REPO = 'git@github.com:jinjin-gsma/plugin-boilerplate.git';
 
 const options = yargs
- .usage("Usage: -n <plugin name>")
- .option("n", { alias: "name", describe: "Name of your plugin", type: "string", demandOption: true })
+ .usage('Usage: -n <plugin name> --no-webpack --no-test')
+ .option('n', { alias: 'name', describe: 'Name of your plugin', type: 'string', demandOption: true })
+ .option('webpack', { alias:'webpack', describe: 'disable webpack and karma tests', type: 'boolean', default: true, demandOption: false })
+ .option('test', { alias:'test', describe: 'disable php unit tests', type: 'boolean', default: true, demandOption: false })
  .argv;
 
-const greeting = `Creating ${options.name}!`;
+const greeting = `Creating ${options.name}! with webpack = ${options.webpack}, test = ${options.test} `;
 
 console.log(greeting);
 
 const shell = require('shelljs');
-shell.exec('git clone git@github.com:jinjin-gsma/plugin-boilerplate.git');
+shell.exec(`git clone ${PLUGIN_REPO}`);
 
 fs.rename('./plugin-boilerplate', `./${options.name}`, function (err) {
     if (err) throw err;
@@ -31,8 +36,10 @@ fs.rename('./plugin-boilerplate', `./${options.name}`, function (err) {
     });
 });
 
-
-const data = { pluginName: options.name };
+const data = {
+    pluginName: options.name,
+    noWebpack: !options.webpack
+};
 
 ejs.renderFile(`./${options.name}/package.json.ejs`, data, (err, result) => {
     if (err) {
@@ -49,9 +56,53 @@ ejs.renderFile(`./${options.name}/package.json.ejs`, data, (err, result) => {
     }
 });
 
+if (!options.webpack) {
+    fs.unlink(`./${options.name}/webpack.config.js`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    fs.unlink(`./${options.name}/karma.conf.js`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    rimraf(`./${options.name}/build`, () => {
+        console.log('build directory Deleted!');
+    });
+}
+
+if (!options.test) {
+    fs.unlink(`./${options.name}/phpunit.xml.dist`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    fs.unlink(`./${options.name}/.phpcs.xml.dist`, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+
+    rimraf(`./${options.name}/tests`, () => {
+        console.log('tests directory Deleted!');
+    });
+
+    rimraf(`./${options.name}/bin`, () => {
+        console.log('bin directory Deleted!');
+    });
+}
+
 fs.unlink(`./${options.name}/package.json.ejs`, (err) => {
   if (err) {
     console.error(err);
     return;
   }
-})
+});
